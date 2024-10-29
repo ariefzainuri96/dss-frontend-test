44935e34281637175c6d3c9bf45508618b0edc0f
Arief Zainuri <flaminglassoo1996@gmail.com>
2024-10-29 22:42:48 +0700

    update entry to /home

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/home',
+                permanent: true, // Set to `false` if you might want to change this redirect in the future
+            },
+        ];
+    },
 };
 
 export default nextConfig;
